b1769767d6a2689a5dd3de83534a0f3d3806a411
Gabo Acosta <gabo.acosta62@gmail.com>
2016-12-05 10:49:37 -0600

    Adds material design to the project

--- app/content.jsx
+++ app/content.jsx
@@ -5,14 +5,64 @@
 import React from 'react';
 import { render } from 'react-dom';
 import LikesComponent from './components/likes.jsx'
+import RaisedButton from 'material-ui/RaisedButton';
+import Dialog from 'material-ui/Dialog';
+import {deepOrange500} from 'material-ui/styles/colors';
+import FlatButton from 'material-ui/FlatButton';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+
+const styles = {
+  container: {
+    textAlign: 'center',
+    paddingTop: 200,
+  },
+};
+
+const muiTheme = getMuiTheme({
+  palette: {
+    accent1Color: deepOrange500,
+  },
+});
+
 
 class App extends React.Component {
-  render () {
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleRequestClose = this.handleRequestClose.bind(this);
+    this.handleTouchTap = this.handleTouchTap.bind(this);
+
+    this.state = {
+      open: false,
+    };
+  }
+
+  handleRequestClose() {
+    this.setState({
+      open: false,
+    });
+  }
+
+  handleTouchTap() {
+    this.setState({
+      open: true,
+    });
+  }
+
+  render() {
+
     return (
-      <div>
-        <p> Hello React!</p>
-        <LikesComponent />
-      </div>
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <div>
+          <AppBar
+            title="Title"
+            iconClassNameRight="muidocs-icon-navigation-expand-more"
+          />
+          <LikesComponent/>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
--- app/index.js
+++ app/index.js
@@ -1,5 +1,10 @@
 /**
  * Created by gabriel.acosta on 11/20/16.
  */
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 require('./content.jsx')
